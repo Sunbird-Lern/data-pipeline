ba6db65a9aaecc395d9c52891ed3e37d386bea61
Jayaprakash8887 <jayaprakash.n@tarento.com>
2023-05-17 11:46:39 +0530

    Issue #LR-451 feat: Local setup documentation update (#126)
    Co-authored-by: Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>

--- notification/notification-job/src/main/scala/org/sunbird/job/notification/domain/Event.scala
+++ notification/notification-job/src/main/scala/org/sunbird/job/notification/domain/Event.scala
@@ -4,8 +4,6 @@ import org.sunbird.job.domain.reader.JobRequest
 
 class Event(eventMap: java.util.Map[String, Any], partition: Int, offset: Long) extends JobRequest(eventMap, partition , offset) {
     
-    import scala.collection.JavaConverters._
-    
     def actor: Map[String, AnyRef] = readOrDefault[Map[String, AnyRef]]("actor", Map[String, AnyRef]())
     def eid: String = readOrDefault[String]("eid", "")
     def action: String = readOrDefault[String]("edata.action", "")
