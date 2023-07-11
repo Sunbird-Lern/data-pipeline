2b069cdc5a35d9a9b5afb38ae0cb82c4813fb241
BharathwajShankar <saialumnibharathwaj@gmail.com>
2023-07-11 12:51:02 +0530

    Issue #LR-511 fix:Issue fix wrt default on Ability for Lern to connect to shared instances

--- lms-jobs/credential-generator/collection-cert-pre-processor/src/main/scala/org/sunbird/job/collectioncert/task/CollectionCertPreProcessorConfig.scala
+++ lms-jobs/credential-generator/collection-cert-pre-processor/src/main/scala/org/sunbird/job/collectioncert/task/CollectionCertPreProcessorConfig.scala
@@ -13,8 +13,8 @@ class CollectionCertPreProcessorConfig(override val config: Config) extends Base
     implicit val stringTypeInfo: TypeInformation[String] = TypeExtractor.getForClass(classOf[String])
     
     //Redis config
-    val collectionCacheStore: Int = config.getInt("redis_collection_cache_index")
-    val contentCacheStore: Int = config.getInt("redis_database_content_cache_index")
+    val collectionCacheStore: Int = config.getInt("redis.collectionCache.id")
+    val contentCacheStore: Int = config.getInt("redis.database.contentCache.id")
     val metaRedisHost: String = config.getString("redis-meta.host")
     val metaRedisPort: Int = config.getInt("redis-meta.port")
 
