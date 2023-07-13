61e4008252546c60ca74725fb5b867e7bd9f7d5d
Vivek-M-08 <vivek.m@pacewisdom.com>
2023-07-13 11:53:36 +0530

    fix for multiple subroles not reflecting under Usersubtype column

--- ml-jobs/program-user-info/src/main/scala/org/sunbird/dp/userinfo/functions/ProgramUserInfoFunction.scala
+++ ml-jobs/program-user-info/src/main/scala/org/sunbird/dp/userinfo/functions/ProgramUserInfoFunction.scala
@@ -89,11 +89,19 @@ class ProgramUserInfoFunction(config: ProgramUserInfoConfig,
      * To get the key-values related to user type
      */
     val userTypeData = new util.HashMap[String, String]
-    if(event.user_Types!= null && event.user_Types.isEmpty==false) {
+    if (event.user_Types != null && event.user_Types.isEmpty == false) {
+      val subTypeBuilder = new StringBuilder
       event.user_Types.forEach(f => {
         userTypeData.put("type", Option(f.get("type")).map(_.toString).filter(_.nonEmpty).orNull)
-        userTypeData.put("sub_type", Option(f.get("subType")).map(_.toString).filter(_.nonEmpty).orNull)
+        val subType = Option(f.get("subType")).map(_.toString).filter(_.nonEmpty).orNull
+        if (subType != null) {
+          if (subTypeBuilder.length() > 0) {
+            subTypeBuilder.append(",")
+          }
+          subTypeBuilder.append(subType)
+        }
       })
+      userTypeData.put("sub_type", if (subTypeBuilder.toString().isBlank) null else subTypeBuilder.toString())
     }
 
     /**
