42d5ae670f1358bd22f94607edfc07eb552fc9a7
anilgupta <ani2824@gmail.com>
2024-04-03 17:28:18 +0530

    Issue #LR-766 test: Unit test

--- jobs-core/src/test/scala/org/sunbird/spec/BaseSpec.scala
+++ jobs-core/src/test/scala/org/sunbird/spec/BaseSpec.scala
@@ -1,24 +1,20 @@
 package org.sunbird.spec
 
 import org.scalatest.{BeforeAndAfterAll, FlatSpec}
-import redis.embedded.{RedisInstance, RedisServer}
+import redis.embedded.RedisServer
 
 class BaseSpec extends FlatSpec with BeforeAndAfterAll {
   var redisServer: RedisServer = _
-//  var redisInstance: RedisInstance = _
 
   override def beforeAll() {
     super.beforeAll()
     redisServer = new RedisServer(6340)
     redisServer.start()
-//    redisInstance = RedisServer.newRedisServer.port(6340).build
-//    redisInstance.start()
     //EmbeddedPostgres.builder.setPort(5430).start() // Use the same port 5430 which is defined in the base-test.conf
   }
 
   override protected def afterAll(): Unit = {
     super.afterAll()
-//    redisInstance.stop()
     redisServer.stop()
   }
 
