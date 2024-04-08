1520a017a71798d221ab724b79ae228fa2b24219
BharathwajShankar <saialumnibharathwaj@gmail.com>
2024-04-08 10:25:35 +0530

    Issue #LR-676 feat:adding some extra logs to debug

--- user-org-jobs/user-ownership-transfer/src/main/scala/org/sunbird/job/ownershiptransfer/domain/Event.scala
+++ user-org-jobs/user-ownership-transfer/src/main/scala/org/sunbird/job/ownershiptransfer/domain/Event.scala
@@ -35,12 +35,15 @@ class Event(eventMap: util.Map[String, Any]) extends BaseEvent(eventMap) {
     if(userId.nonEmpty) {
       val url = config.userOrgServiceBasePath + config.userReadApi + "/" + userId + "?identifier,rootOrgId"
       val userReadResp = httpUtil.get(url)
-
+      println(s"User read response: ${userReadResp.body}")
       if (200 == userReadResp.status) {
         metrics.incCounter(config.apiReadSuccessCount)
         val response = JSONUtil.deserialize[Map[String, AnyRef]](userReadResp.body)
         val userDetails = response.getOrElse("result", Map[String, AnyRef]()).asInstanceOf[Map[String, AnyRef]].getOrElse("response", Map[String, AnyRef]()).asInstanceOf[Map[String, AnyRef]]
-        userDetails.getOrElse("identifier", "").asInstanceOf[String].equalsIgnoreCase(userId) && userDetails.getOrElse("rootOrgId","").asInstanceOf[String].equalsIgnoreCase(organisation)
+        println(s"User details: $userDetails")
+        val comparisonResult = userDetails.getOrElse("identifier", "").asInstanceOf[String].equalsIgnoreCase(userId) && userDetails.getOrElse("rootOrgId", "").asInstanceOf[String].equalsIgnoreCase(organisation)
+        println(s"Comparison result: $comparisonResult")
+        comparisonResult
       } else false
     } else false
   }
