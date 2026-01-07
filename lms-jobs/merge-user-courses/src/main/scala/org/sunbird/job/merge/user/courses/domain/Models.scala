d3107e05df20b1d4cb52ce8c770fba5f3c767482
Rakshitha-D <rakshithagowda.d@gmail.com>
2026-01-07 17:02:51 +0530

    removed merge-user-courses job

--- lms-jobs/merge-user-courses/src/main/scala/org/sunbird/job/merge/user/courses/domain/Models.scala
+++
@@ -1,9 +0,0 @@
-package org.sunbird.job.merge.user.courses.domain
-
-class Models {}
-
-case class BatchEnrollmentSyncModel(batchId: String, userId: String, courseId: String) {
-  def this() = this("", "", "")
-}
-
-
