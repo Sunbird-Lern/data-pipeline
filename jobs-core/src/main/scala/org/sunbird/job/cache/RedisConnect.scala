bab5808bb0882384d5e80c31f540e800dc37e125
chethann007 <gchethan7602@gmail.com>
2026-03-24 15:36:54 +0530

    fix: use safe hasPath-guarded redis host/port defaults in RedisConnect

--- jobs-core/src/main/scala/org/sunbird/job/cache/RedisConnect.scala
+++ jobs-core/src/main/scala/org/sunbird/job/cache/RedisConnect.scala
@@ -1,6 +1,5 @@
 package org.sunbird.job.cache
 
-import com.typesafe.config.Config
 import org.slf4j.LoggerFactory
 import org.sunbird.job.BaseJobConfig
 import redis.clients.jedis.Jedis
@@ -9,10 +8,9 @@ class RedisConnect(jobConfig: BaseJobConfig, host: Option[String] = None, port: 
 
   private val serialVersionUID = -396824011996012513L
 
-  val config: Config = jobConfig.config
   val redisEnabled: Boolean = jobConfig.redisEnabled
-  val redisHost: String = host.getOrElse(Option(config.getString("redis.host")).getOrElse("localhost"))
-  val redisPort: Int = port.getOrElse(Option(config.getInt("redis.port")).getOrElse(6379))
+  val redisHost: String = host.getOrElse(jobConfig.redisHost)
+  val redisPort: Int = port.getOrElse(jobConfig.redisPort)
   private val logger = LoggerFactory.getLogger(classOf[RedisConnect])
 
   private def getConnection(backoffTimeInMillis: Long): Jedis = {
